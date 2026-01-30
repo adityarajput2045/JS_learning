082da730213ff3b88cc0dc6d8fb800aa898c347d
Aditya Kumar <adityarajput2045@gmail.com>
2026-01-30 13:21:01 +0000

    Operators in JS

--- 02_basics/practice2.js
+++ 02_basics/practice2.js
@@ -1,6 +1,65 @@
-// Operator 
-let a = 30
-let b = 20
+// Operator
+let a = 2, b = 3
 
-// Arithmetic 
+//Arithmetic
+// + - * / ** %
+
+let sum = a + b
+console.log("Sum",(sum))
+
+let sub = a + b
+console.log("Subtract",(sub))
+
+let mul = a * b
+console.log("Multiplication",(mul))
+
+let div = a / b
+console.log("Divide",(div))
+
+let modulo = a % b
+console.log("Modulo",(modulo))
+
+let aToThePowerb = a**b
+console.log("A to the power b",aToThePowerb)
+
+//Comparison Operator
+//>, =>, <, <=, ==, ===
+
+const m = 7, n = 8
+
+console.log(m > n)  // False
+console.log(m >= n) // False
+console.log(m < n)  // True
+console.log(m <= n)  // True
+console.log(m == n)  // False
+
+// Difference between == and ===
+// == does not check the datatypes and === can check element and datatype as well
+console.log(7 == "7") // True
+console.log(7 === "7") // False
+console.log(7 === 7)  // True
+
+//Logical Operator
+// and operator
+console.log(false && false) //false
+console.log(false && true) //false
+console.log(true && true) //true
+console.log(true && false) //false
+
+// or operator
+console.log(false || false) //false
+console.log(false || true) //true
+console.log(true || true) //true
+console.log(true || false) //true
+
+//Bitwise operator
+//bitwise and  &
+//bitwise or  |
+//bitwise not  -
+//bitwise xor  ^
+
+console.log(5 & 7) // 5
+console.log(8 | 5) // 13
+console.log(5 - 7)  // -2
+console.log(5 ^ 7)  // 2
 
